30bce67e63070a8c7f1d46f4aaa8da277f1e6188
Schousen <49717456+Schousen@users.noreply.github.com>
2019-05-12 13:00:02 +0200

    added registerTimemedtode

--- SoftwareHuset/src/softwareApp/UserLogin.java
+++ SoftwareHuset/src/softwareApp/UserLogin.java
@@ -7,6 +7,27 @@ public class UserLogin
 {
 	private static ArrayList<Project> nullAsProjectList = new ArrayList<Project>();
 	private static ArrayList<Project> nullProjectList = new ArrayList<Project>();
+	
+	public static void registerUser(String userName) throws Exception
+	{
+		assert true; // precondition
+		boolean userExist = checkifuserexist(userName);
+		if (userName.matches("^[A-Z]*$") && userName.length() >=
+				1 && userName.length() < 5 && userExist == false)
+		{
+			assert (userName.matches("^[A-Z]*$") && userName.length() >=
+					1 && userName.length() < 5 && userExist == false); //Precondition
+			User user = new User(userName,nullProjectList,
+					nullAsProjectList);
+			userManagement.addUser(user);
+			assert (userManagement.companyList.contains(user)); //postcondition
+		}
+		else
+		{
+			throw new IllegalArgumentException("User name cannot contain numbers,"
+					+ " and should have a length between one and four.");
+		}
+	}
 
 	public static void loginUser(String userName) throws Exception
 	{
---
+++ SoftwareHuset/test/softwarehuset/acceptance_tests/User_Registration.java
@@ -0,0 +1,60 @@
+package softwarehuset.acceptance_tests;
+import static org.junit.Assert.assertEquals;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import softwareApp.*;
+import softwarePersistence.*;
+public class User_Registration {
+
+	private String userName;
+	private String actualAnswer;
+	private boolean succeedOr;
+	public User_Registration() {
+		
+		testData.removeAllData();
+		//The project with vacation, courses and sickness activities
+		projectManagement.createDefaultProject();
+		//Spawning test data. Executing phase shift
+		testData.projectData();
+		testData.activityData();
+		testData.userData();
+	}
+	
+	@Given("^a new user with id \"([^\"]*)\"$")
+	public void aNewUserWithId(String userName) {
+		this.userName = userName;
+	}
+
+	@When("^users tries to be registred$")
+	public void usersTriesToBeRegistred() {
+		try {
+			UserLogin.registerUser(userName);
+			succeedOr = true;
+		} catch (Exception e) {
+			actualAnswer = e.getMessage();
+			succeedOr = false;
+		}
+	}
+
+	@Then("^the user registration succeeds$")
+	public void theUserRegistrationSucceeds() {
+		assertEquals(true,succeedOr);
+	}
+
+	@Then("^the user registration fails, and get message \"([^\"]*)\"$")
+	public void theUserRegistrationFailsAndGetMessage(String answer) {
+		assertEquals(answer,actualAnswer);
+	}
+
+	@Then("^the user \"([^\"]*)\" can be found in the companylist$")
+	public void theUserCanBeFoundInTheCompanylist(String userName) {
+		assertEquals(true,UserLogin.checkifuserexist(userName));
+	}
+
+	@Then("^the user \"([^\"]*)\" cannot be found in the companylist$")
+	public void theUserCannotBeFoundInTheCompanylist(String userName) {
+	assertEquals(false,UserLogin.checkifuserexist(userName));
+	}
+
+}
