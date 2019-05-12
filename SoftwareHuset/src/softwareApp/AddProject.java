3ebc608c25c2296e943af5e30800d95c6fcf62ae
Schousen <49717456+Schousen@users.noreply.github.com>
2019-05-12 13:04:45 +0200

    Addproject

---
+++ SoftwareHuset/src/softwareApp/AddProject.java
@@ -0,0 +1,39 @@
+package softwareApp;
+
+import java.util.ArrayList;
+
+import softwareDomain.Activity;
+import softwareDomain.Project;
+import softwareDomain.User;
+import softwarePersistence.*;
+
+public class AddProject {
+	
+	private static User nullUser;
+	private static ArrayList<User> nullUserList = new ArrayList<User>();
+	private static ArrayList<Activity> nullActivityList = new ArrayList<Activity>();
+	
+	public static void addProjectToSystem(String startDay, String startMonth, String startYear,
+			String endDay, String endMonth, String endYear, String time)
+	{
+		assert true; //precondition
+		if (checkLegalInput(startDay,startMonth,  startYear,
+				 endDay, endMonth, endYear, time))
+		{
+			assert startDay!=null && startDay.length()==2 && startMonth!=null &&
+					startMonth.length()==2 && startYear!=null && startYear.length()==4;  // precondtion
+			assert endDay!=null && endDay.length()==2 && endMonth!=null &&
+					endMonth.length()==2 && endYear!=null && endYear.length()==4; // precondition
+			assert time !=null; //precondition
+			String name = startYear.substring(2)+"000"+(projectManagement.projectList.size()+1);
+			String startDate = startDay+"/"+startMonth+"-"+startYear;
+			String endDate = endDay+"/"+endMonth+"-"+endYear;
+			Project newProject = new Project(name,startDate,endDate,
+					0, Integer.parseInt(time), nullUserList, nullUser, nullActivityList);
+					projectManagement.addProject(newProject);
+			assert projectManagement.projectList.contains(newProject);  //postCondition
+		}
+	}
+	
+
+}
